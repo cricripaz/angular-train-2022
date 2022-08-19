10a1ed81ac5af38aeb1ede5a46f78cbbdf88c077
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-08-19 18:10:29 -0400

    [19-08-2022 task 1] new angular 14 import component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hola ';
+
+  products = [
+    {nombre: "polera", talla: 13, stock: 12, tipo: "calor"},
+    {nombre: "camisa", talla : 54, stock: 22, tipo: "frio"},
+    {nombre: "jeans", talla : 78, stock: 0, tipo: "calor"},
+    {nombre: "corto", talla : 90, stock: 44, tipo: "calor"},
+    {nombre: "buso", talla : 56, stock: 0, tipo: "frio"},
+    {nombre: "blusa", talla : 33, stock: 50, tipo: "frio"},
+    {nombre: "chompa", talla : 10, stock: 5, tipo: "calor"},
+    {nombre: "bozer", talla : 34, stock: 1, tipo: "frio"},
+    {nombre: "blusa", talla : 5, stock: 2, tipo: "calor"},
+    {nombre: "guantes", talla : 75, stock: 5, tipo: "calor"}
+  ]
+
+
+  hotProducts : any = []
+  coolProducts : any = []
+
+
+  constructor( ) {
+
+  }
+
+  ngOnInit(){
+
+    // this.products.forEach( p => {
+    //   if(p.tipo === 'calor'){
+    //     this.hotProducts.push(p)
+    //   }else {
+    //     this.coolProducts.push(p)
+    //   }
+    // })
+
+
+    this.hotProducts = this.products.filter( p => p.tipo === 'calor' );
+    this.coolProducts = this.products.filter( p => p.tipo === 'frio' );
+
+  }
+
+
 }
