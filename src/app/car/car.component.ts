756e8b0c145bf90647ca97893cc5b7e61f6953e6
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-08-22 18:56:39 -0400

    commit primer parcial cristian paz

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,53 @@
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {AppComponent} from "../app.component";
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.scss']
+})
+export class CarComponent implements OnInit {
+
+
+  @Input() type!:string;
+
+  @Input() color!:string;
+
+  @Input() capacity!:number;
+
+  @Input() createDate!:string;
+
+  @Input() price!:number;
+
+  @Input() isEnableButtonBuy!:boolean ;
+
+  @Input() isHiddenText: boolean = true;
+
+  @Input() moneyType!: string ;
+
+  @Input() category!: string ;
+
+
+  constructor(@Inject(AppComponent) private parent: AppComponent) { }
+
+  ngOnInit(): void {
+
+
+  }
+
+  buyClick() {
+
+    this.isEnableButtonBuy = true;
+    this.isHiddenText = false
+
+    if ( this.category === 'sale' ){
+      this.parent.totalBoliviansNewCars += this.price
+    }else {
+      this.parent.totalBoliviansUsedCars += this.price
+    }
+
+
+
+
+  }
+}
