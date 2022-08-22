b90156e1abfa6af1499a5b6e668f4502ef3df00f
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-08-22 18:57:32 -0400

    commit primer parcial cristian paz

---
+++ src/app/date-format.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: string):any {
+
+    let d = new Date(value);
+    return d.toLocaleDateString('en-GB'); // dd/mm/yyyy
+  }
+
+
+
+}
+
