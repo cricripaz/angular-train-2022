00d85efa2cf3ec23ecc02b486e1a45574b561380
cricripaz <cricripaz6@gmail.com>
2022-08-22 11:55:29 -0400

    [05-08-2022 task 1] Arrow functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hola ';
+  public title = 'test';
+
+  constructor() {
+    console.log('hi -  hola ');
+  }
+
+  private getName(name: string): string {
+    return 'hi' + name;
+  }
+
+  // --- Arrow function
+  // const getName = (name: string) => {return "hi" + name;}
+  // const getName = (name: string) => "hi" + name
+  // const getName = () => "hi"
+  // const getName = (name:string, lasName:string) => lasName
+
+  // const mult = num => num*2
+  private mult(num: any) {
+    return num;
+  }
 }
