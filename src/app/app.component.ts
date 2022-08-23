167270476c4df0c33c2f07153b2c62c1d8420b91
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-08-23 17:35:30 -0400

    [23-08-2022 task 1] pure and impure functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hola ';
+
+  constructor() {
+    console.log(this.pureExample(3,3))
+    console.log(this.pureExample(2,3))
+    console.log(this.pureExample(3,11))
+    console.log(this.pureExample(23,3))
+
+    console.log('Impure')
+    console.log(this.impureExample(3,3))
+    console.log(this.impureExample(2,3))
+    console.log(this.impureExample(3,11))
+    console.log(this.impureExample(23,3))
+
+  }
+
+  pureExample(a : number , b:number):number{
+
+    return a+b;
+
+  }
+
+  impureExample(a:number , b:number){
+    return (a + b + Math.random()).toFixed()
+  }
 }
