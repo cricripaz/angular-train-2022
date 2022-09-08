d13507c861f706cdfab687d032bbdc89734a6d05
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-09-08 18:51:10 -0400

    [08-09-2022 task 2] authentication with JWT

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  onLogin() {
+    this.authService.login({
+      email: 'test@test.com',
+      password: '123456',
+      returnSecureToken: true
+    }).subscribe(res => {
+      console.log('RESPONSE: ', res)
+    })
+  }
 }
--- src/app/pages/services/auth.service.ts
+++ src/app/pages/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  login(body: any): Observable<any> {
+    return this.http.post(environment.apiLogin+environment.apiKey, body)
+  }
 }
