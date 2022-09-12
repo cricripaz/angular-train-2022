49de261c8da34dfe8472c96d7955caba3050e12e
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-09-12 18:04:29 -0400

    [09-09-2022 task 1] auth interceptor part 1

--- src/app/pages/services/auth.service.ts
+++ src/app/pages/services/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,39 @@ export class AuthService {
   private url = environment.auth.apiBaseUrl;
   private apiKey = environment.auth.apiKey;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private router: Router) {
   }
 
   login(body: any): Observable<any> {
-    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.apiKey}`, body)
+    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.apiKey}`, body).pipe(
+        map((res: any) => {
+          this.authSuccess(res.idToken, res.localId)
+          return res
+        })
+    )
   }
 
   createUser(body: any): Observable<any> {
     return this.http.post(`${this.url}/v1/accounts:signUp?key=${this.apiKey}`, body)
+  }
+
+  private authSuccess(token: string, userId: string): void {
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId)
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  public getUserId(): string | null {
+    return localStorage.getItem('userId');
+  }
+
+  public logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    this.router.navigate(['login'])
   }
 
 }
