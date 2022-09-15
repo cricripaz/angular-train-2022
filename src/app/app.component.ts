f548b0858285a543ec5821a6c1f9d43e2d4e79cb
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-09-15 16:37:48 -0400

    [02-09-2022 task 3] DELETE and UPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ProductService} from "./product.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hola ';
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit() {
+    this.productService.getAllProducts().subscribe(res => {
+      console.log('RESPONSE: ', res)
+    })
+
+
+
+    this.productService.createProduct({
+      description: 'soy la descripcion',
+      imageUrl: 'soy la imagen',
+      ownerId: 1,
+      price: 20,
+      title: 'Polera',
+    }).subscribe(res => {
+      console.log('RESPONSE POST: ', res)
+    })
+
+    this.productService.deleteProduct('p2').subscribe(res => {
+      console.log('RESPONSE DELETE: ', res)
+    })
+
+
+    this.productService.updateProduct('p9', {
+      description: 'modificado',
+      imageUrl: 'modificadon',
+      ownerId: 1000000,
+      price: 20000000,
+      title: 'modificado',
+    }).subscribe(res => {
+      console.log('RESPONSE UPDATE: ', res)
+    })
+
+  }
+
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {environment} from "../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private url:string = environment.app.apiUrlBase;
+
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getAllProducts(): Observable<any> {
+    return this.http.get(
+        `${this.url}/products.json`).pipe(
+        map(s => Object.entries(s)),
+        map(s => s.map(s => ({id: s[0], ...s[1]}))))
+  }
+
+
+  public createProduct(body: any): Observable<any> {
+    return this.http.post(`${this.url}/products.json`, body)
+  }
+
+  public deleteProduct(id: any): Observable<any> {
+    return this.http.delete(`${this.url}/products/${id}.json`)
+  }
+
+  public updateProduct(id: any, body: any): Observable<any> {
+    return this.http.put(`${this.url}/products/${id}.json`, body)
+  }
+
+
+
+}
